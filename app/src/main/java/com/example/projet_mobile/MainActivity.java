4cf65258f0c67445b5f646edb84880d6c26e8603
lwasterl <33127211+lwasterl@users.noreply.github.com>
2019-11-04 11:59:49 +0100

    Bouton cliquable

--- app/src/main/java/com/example/projet_mobile/MainActivity.java
+++ app/src/main/java/com/example/projet_mobile/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.projet_mobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,8 +8,11 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -21,10 +25,38 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.draw_layout);
+        NavigationView navigationView =findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if(savedInstanceState ==null){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new MapFragment()).commit();
+        navigationView.setCheckedItem(R.id.nav_map);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_map:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MapFragment()).commit();
+                break;
+            case R.id.nav_iti:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ItiFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 
     @Override
