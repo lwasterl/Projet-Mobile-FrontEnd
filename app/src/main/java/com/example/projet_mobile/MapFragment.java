fd3bb35b19cefbf106456ec94881215336691062
lwasterl <33127211+lwasterl@users.noreply.github.com>
2019-11-05 14:25:27 +0100

    Add google maps

--- app/src/main/java/com/example/projet_mobile/MapFragment.java
+++ app/src/main/java/com/example/projet_mobile/MapFragment.java
@@ -1,5 +1,7 @@
 package com.example.projet_mobile;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,13 +9,47 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-public class MapFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private static final LatLng Namur = new LatLng(50.4674, 4.8720); //Your LatLong
+    private GoogleMap mMap;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        return view;
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // centre la caméra sur namur
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Namur));
+        mMap.setMinZoomPreference(14.0f);
+        mMap.setMaxZoomPreference(29.0f);
     }
 }
+
