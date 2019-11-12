18249f2f4d99eee0c314a651ac7b309086cf4b7e
lwasterl <33127211+lwasterl@users.noreply.github.com>
2019-11-12 13:27:20 +0100

    Merge branch 'master' of C:\Users\HP\Documents\ProjetMobile with conflicts.

--- app/src/main/java/com/example/projet_mobile/MapFragment.java
+++ app/src/main/java/com/example/projet_mobile/MapFragment.java
@@ -1,19 +1,96 @@
 package com.example.projet_mobile;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-public class MapFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private static final LatLng Namur = new LatLng(50.4674, 4.8720); //Your LatLong
+    private GoogleMap mMap;
+    private Button filter;
+    String[] listItems = {"Afficher le traffic"};
+    boolean[] checkedItems = new boolean[]{false};
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        filter = (Button) view.findViewById(R.id.filter);
+        filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(getContext());
+                alertDialog.setTitle("Choisissez vos filtres :");
+                alertDialog.setMultiChoiceItems(listItems, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        switch (which){
+                            case 0:
+                                mMap.setTrafficEnabled(isChecked);
+                                break;
+                        }
+                        Toast.makeText(getContext(), "Filtre Mis à jour" , Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                alertDialog.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog dialog = alertDialog.create();
+                dialog.show();
+
+
+            }
+        });
+        return view;
     }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // centre la caméra sur namur
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Namur));
+        mMap.setMinZoomPreference(14.0f);
+        mMap.setMaxZoomPreference(29.0f);
+
+
+    }
+
 }
+
